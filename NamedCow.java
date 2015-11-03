0e9e84f06bde81c17128573a9c53aab7e4e2177f
ChickenofDooom <dexterity728@gmail.com>
2015-11-03 10:56:51 -0800

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,22 @@
-class NamedCow
-{
-
+class NamedCow extends Cow
+/*{ private String myName;
+public NamedCow(String name){
+  myName=name;
+}
+public NamedCow(){
+ myName="Betsy"; 
+}
+ public String getName(){return myName;}
+}*/
+ {   protected String myName;
+     public NamedCow(String type, String sound,String name)     {         
+         myType = type;         
+         mySound = sound;  
+         myName = name;   
+     }     
+     public NamedCow()     {         
+         myName = "unknown";    
+     }      
+    
+     public String getName(){return myName;}
 }
